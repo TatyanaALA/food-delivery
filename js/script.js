ac9f97ed8836824c2b9882a2c0974927f60d2ab8
Tatyana Rubtsova <liotic_ala@list.ru>
2023-08-21 12:17:10 +0600

    added Fetch API

--- js/script.js
+++ js/script.js
@@ -243,10 +243,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			`;
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');
-
-			request.setRequestHeader('Content-type', 'application/json');
 			const formData = new FormData(form);
 
 			const object = {};
@@ -254,20 +250,23 @@ window.addEventListener('DOMContentLoaded', () => {
 				object[key] = value;
 			});
 
-			const json = JSON.stringify(object);
-
-			request.send(json);
-
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
-					showThanksModal(message.success);
-					form.reset();
-					statusMessage.remove();
-				} else {
-					showThanksModal(message.failur);
-				}
+			fetch('server.php', {
+				method: "POST",
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(object)
 			})
+			.then(data => data.text())
+			.then(data => {
+				console.log(data);
+				showThanksModal(message.success);
+				statusMessage.remove();
+			}).catch(() => {
+				showThanksModal(message.failur);
+			}).finally(() => {
+				form.reset();
+			});
 		});
 	}
 
@@ -295,3 +294,4 @@ window.addEventListener('DOMContentLoaded', () => {
 		}, 4000);
 	}
 });
+
