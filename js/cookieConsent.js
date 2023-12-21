91e9aaba90cb10f95a8fe1a96b654f8476a07748
Tatyana <tanya_r@list.ru>
2023-12-21 16:02:12 +0600

    classes for cookie

--- js/cookieConsent.js
+++ js/cookieConsent.js
@@ -1,43 +1,72 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  const cookieStorage = {
-    getItem: (key) => {
+  class CookieConsent {
+    constructor({popup, btnConfirm, btnCancel, activeClass = ''} = {}) {
+      this.popup = document.querySelector(popup);
+      this.btnConfirm = document.querySelector(btnConfirm);
+      this.btnCancel = document.querySelector(btnCancel);
+      this.activeClass= activeClass;
+      this.consentPropertyType = 'site_consent';
+    }
+
+    getItem = (key) => {
       const cookies = document.cookie.split(';')
                                      .map(cookie => cookie.split('='))
                                      .reduce((acc, [key, value]) => ({...acc, [key.trim()] : value}), {});
-    
-      return cookies[key];
-    },
-    setItem: (key, value) => {
-      document.cookie = `${key}=${value};expires=Sun, 16 Jul 2025 06:23:41 GMT`;
-    }
-  };
-  
-  const storageType = cookieStorage;
-  const consentPropertyType = 'site_cobsent';
-  
-  const hasConsented = () => storageType.getItem(consentPropertyType) === "true" ? true : false;
-  const toggleStorage = (prop) => storageType.setItem(consentPropertyType, prop);
 
-  const popup = document.querySelector('.popup'),
-        btnConfirm = document.querySelector('[data-confirm]'),
-        btnCancel = document.querySelector('[data-cancel]');
-  
-  if (hasConsented()) {
-    console.log('Loading...')
-  } else {
-    popup.classList.add('popup_active');
+      return cookies[key]; 
+    }
+
+    setItem = (key, value) => {
+      document.cookie = `${key}=${value};expires=Sun, 16 Jul 2024 06:23:41 GMT`;
+    }
+
+    hasConsented = () => this.getItem(this.consentPropertyType) === 'true' ? true : false;
+
+    changeStatus = (prop) => {
+      this.setItem(this.consentPropertyType, prop);
+      if (this.hasConsented()) {
+        // Подписка на сервисы
+        myScripts(); 
+      }
+    }
+
+    bindTriggers = () => {
+      this.btnConfirm.addEventListener('click', () => {
+        this.changeStatus(true);
+        this.popup.classList.remove(this.activeClass);
+      });
+
+      this.btnCancel.addEventListener('click', () => {
+        this.changeStatus(false);
+        this.popup.classList.remove(this.activeClass);
+      });
+    }
+
+    init = () => {
+      try {
+        if (this.hasConsented()) {
+          myScripts();
+        } else {
+          this.popup.classList.add(this.activeClass);
+        }
+
+        this.bindTriggers();
+      } catch(e) {
+        console.error('Переданы не все данные');
+      }
+    }
   }
 
-  btnConfirm.addEventListener('click', () => {
-    toggleStorage("true");
-    popup.classList.remove('popup_active');
-    console.log('Loading...')
-  });
+  new CookieConsent({
+    popup: '.popup',
+    btnConfirm: '[data-confirm]',
+    btnCancel: '[data-cancel]',
+    activeClass: 'popup_active'
+  }).init();
 
-  btnCancel.addEventListener('click', () => {
-    toggleStorage(false);
-    popup.classList.remove('popup_active');
-  });
+  function myScripts() {
+    console.log('Loading...');
+  }
 });
